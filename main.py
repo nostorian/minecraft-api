b4e8a58dc0297893151b8c8294ceb11dcdb4fc12
fw-real <nostorian@proton.me>
2024-05-08 02:26:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ async def chat_to_bot(msg: Message, authorized: bool = Depends(authenticate)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0", loop='asyncio')
